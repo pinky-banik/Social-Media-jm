373dd83043d942327e685f25ea7589ded6fc3de0
pinky-banik <pinky.rani.banik@gmail.com>
2023-11-05 02:56:03 +0600

    context api

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,16 +14,25 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
-import { createUserAccount } from "@/lib/appwrite/api";
 import { useToast } from "@/components/ui/use-toast"
+import { useCreateUserAccount, useSigninAccount } from "@/lib/react-query/queriesAndMutations";
 
 
 
 
 const SignupForm = () => {
 
-    const isLoding = false;
     const { toast } = useToast()
+
+    const {
+        mutateAsync: createUserAccount,
+        isLoading: isCreatingUser
+    } = useCreateUserAccount();
+
+    const {
+        mutateAsync : signInAccount ,
+        isLoading : isSigningIn ,
+    } = useSigninAccount();
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof SignupValidation>>({
@@ -36,16 +45,27 @@ const SignupForm = () => {
         },
     })
 
+
+
     // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof SignupValidation>) {
         const newUser = await createUserAccount(values);
-        if(!newUser){
+        if (!newUser) {
             return toast({
                 title: "Sign Up failed, Please try again.",
-              })
+            })
         }
 
-        const session =await signInAccount()
+        const session = await signInAccount({
+            email : values.email,
+            password :  values.password,
+        })
+
+        if(!session){
+            return toast({ title:'Sign in failed. Please try again.'})
+        }
+
+        
     }
     return (
 
@@ -55,71 +75,71 @@ const SignupForm = () => {
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
                 <p className="text-light-3 small-medium md:base-regular mt-2">To use Snapgram, please enter your account details</p>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full  mt-4">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input type="text" className="shad-input" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input type="text" className="shad-input" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input type="text" className="shad-input" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input type="text" className="shad-input" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button className="shad-button_primary" type="submit">
-                    {
-                        isLoding? (
-                        <div className="flex-center gap-2 ">
-                            <Loader/> Loading...
-                        </div>
-                        ) : "Sign up"
-                    }
-                </Button>
-                <p className="text-small-regular text-light-2 text-center mt-2">
-                    Already have an account?  <Link className="text-primary-500 text-small-semibold ml-1" to="/sign-in">Signin</Link>
-                </p>
-            </form>
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button className="shad-button_primary" type="submit">
+                        {
+                            isCreatingUser ? (
+                                <div className="flex-center gap-2 ">
+                                    <Loader /> Loading...
+                                </div>
+                            ) : "Sign up"
+                        }
+                    </Button>
+                    <p className="text-small-regular text-light-2 text-center mt-2">
+                        Already have an account?  <Link className="text-primary-500 text-small-semibold ml-1" to="/sign-in">Signin</Link>
+                    </p>
+                </form>
             </div>
         </Form>
 
---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,18 @@
+import { INewUser } from "@/types"
+import { createUserAccount, signInAccount } from "../appwrite/api"
+import { useMutation } from '@tanstack/react-query'
+
+export const useCreateUserAccount = () => {
+    return useMutation({
+        mutationFn: (user: INewUser) => createUserAccount(user)
+    })
+}
+
+export const useSigninAccount = () => {
+    return useMutation({
+        mutationFn: (user: {
+            email: string;
+            password: string
+        }) => signInAccount(user)
+    })
+}
