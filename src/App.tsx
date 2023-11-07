24e68829778b7e1d6b0870303b2228f7f1315111
pinky-banik <pinky.rani.banik@gmail.com>
2023-11-07 21:29:26 +0600

    sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SigninForm from './_auth/forms/SigninForm';
-import { Home } from "./_root/Pages";
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from "./_root/Pages";
 import SignupForm from "./_auth/forms/SignupForm";
 import Authlayout from "./_auth/Authlayout";
 import RootLayout from "./_root/RootLayout";
@@ -23,6 +23,14 @@ const App = () => {
                 {/* Private Route */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
 
             </Routes>
